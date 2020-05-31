478327542e511c18f71b8fc48545f5b07e4c0fa6
tangbo <tangbo2@100tal.com>
2020-05-31 21:36:59 +0800

    leetcode 二叉树

--- src/components/array/ArrayContent.vue
+++ src/components/array/ArrayContent.vue
@@ -70,6 +70,13 @@ export default {
                     stemContent:`运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。写入数据 put(key, value) - 如果密钥已经存在，则变更其数据值；如果密钥不存在，则插入该组「密钥/数据值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。`,
                     answerCode:String(arrayAnswer[7])
                 },
+                {
+                    id:8,
+                    stemTitle:`101. 对称二叉树`,
+                    level:"简单",
+                    stemContent:`给定一个二叉树，检查它是否是镜像对称的。`,
+                    answerCode:String(arrayAnswer[8])
+                },
             ]
         }
     }
--- src/leecodejs/array/index.js
+++ src/leecodejs/array/index.js
@@ -160,7 +160,28 @@ class LRUCache {
     }
 }
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    const check = (left, right) => {   
+      if (!left && !right) return true; 
+      if (left && right) {           
+        return left.val === right.val && check(left.left, right.right) && check(left.right, right.left) 
+      }
+      return false;  
+    }
+    return !root || check(root.left, root.right) 
+  };
+  
 
 
-
-export default [removeDuplicates,checkPermutation,rotateMatrix,singleNumber,replaceSpaces,pivotIndex,buildTree,LRUCache]
+export default [removeDuplicates,checkPermutation,rotateMatrix,singleNumber,replaceSpaces,pivotIndex,buildTree,LRUCache,isSymmetric]
